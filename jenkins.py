8816dca89b9ed1a7f9833af75ee47585a8507f80
nitishtlgt <nitishtlgt@gmail.com>
2023-12-06 15:09:04 +0530

    initial commit

---
+++ jenkins.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def browser():
+    options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver = webdriver.Chrome()
+    yield driver
+
+def test_github_login(browser):
+    browser.get("https://www.google.com/")
+
+    search_field = browser.find_element("xpath", '//*[@id="APjFqb"]')
+
+    search_field.send_keys("Virat kohli")
+    search_field.send_keys(Keys.ENTER)
+
+    time.sleep(2)
+
+    assert browser.find_element("xpath", '//*[@id="rcnt"]/div[2]/div/div/div[3]/div[1]/div/div/div/div[1]/div/div')
+
+if __name__ == "__main__":
+    pytest.main([__file__])
